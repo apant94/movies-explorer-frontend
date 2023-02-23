0cfd8abfce6a116958efd15606ed809d5cca4475
Anna Panteleeva <apanteleeva94@gmail.com>
2023-02-23 18:58:32 +0300

    feat: добавил верстку Promo

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -3,7 +3,7 @@ import './AboutProject.css';
 function AboutProject() {
   return (
   <section className='aboutproject'>
-    
+    <h2 className='aboutproject__title'>О проекте</h2>
   </section>
   );
 }
