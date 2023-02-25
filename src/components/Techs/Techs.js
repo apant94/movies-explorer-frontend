51335ba5c423aab89e5d12edf97fe904fdddf7d6
Anna Panteleeva <apanteleeva94@gmail.com>
2023-02-25 15:08:00 +0300

    feat: создает блок techs

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -3,7 +3,20 @@ import './Techs.css';
 function Techs() {
   return (
   <section className='techs'>
-    
+    <div className='techs__container'>
+      <h2 className='techs__title'>Технологии</h2>
+      <h3 className='techs__subtitle'>7 технологий</h3>
+      <p className='techs__definition'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+      <div className='techs__blocks'>
+        <div className='techs__block'><p className='techs__block-text'>HTML</p></div>
+        <div className='techs__block'><p className='techs__block-text'>CSS</p></div>
+        <div className='techs__block'><p className='techs__block-text'>JS</p></div>
+        <div className='techs__block'><p className='techs__block-text'>React</p></div>
+        <div className='techs__block'><p className='techs__block-text'>Git</p></div>
+        <div className='techs__block'><p className='techs__block-text'>Express.js</p></div>
+        <div className='techs__block'><p className='techs__block-text'>mongoDB</p></div>
+      </div>
+    </div>
   </section>
   );
 }
