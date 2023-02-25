ef6b204e06f42b04126291d6807ab6c4687d35a9
Anna Panteleeva <apanteleeva94@gmail.com>
2023-02-25 19:07:56 +0300

    feat: добавляет блок Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,16 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className='footer'>
-    
+      <p className='footer__description'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className='footer__container'>
+        <p className='footer__copyright'>© 2023</p>
+        <nav className='footer__links'>
+          <ul className='footer__list'>
+            <li><a className='footer__link-item' href='https://practicum.yandex.ru/'>Яндекс.Практикум</a></li>
+            <li><a className='footer__link-item' href='https://github.com/apant94'>Github</a></li>
+          </ul>
+        </nav>
+      </div>
     </footer>
   );
 }
