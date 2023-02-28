a1b01b1ab3663b133197059502c53af2011416e8
Anna Panteleeva <apanteleeva94@gmail.com>
2023-02-28 16:40:03 +0300

    feat: приклеивает футер

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,11 @@
 import './Profile.css';
 
 function Profile() {
-
+  return(
+    <main className='profile'>
+      <h2 className='profile__greetings'>Привет, Виталий!</h2>
+    </main>
+  )
 }
 
 export default Profile;
