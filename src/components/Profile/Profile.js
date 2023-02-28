a7cdd7023aaec7d24f6084cb6b4290f64c0bf0e6
Anna Panteleeva <apanteleeva94@gmail.com>
2023-02-28 19:16:33 +0300

    feat: добавляет Profile форму

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,6 +4,16 @@ function Profile() {
   return(
     <main className='profile'>
       <h2 className='profile__greetings'>Привет, Виталий!</h2>
+      <form className='profile__form'>
+        <div className='profile__input-wrapper'>
+          <label for='name' className='profile__input-label'>Имя</label>
+          <input className='profile__input' name='name' type='text' value='Виталий' />
+        </div>
+        <div className='profile__input-wrapper'>
+          <label for='email' className='profile__input-label'>E-mail</label>
+          <input className='profile__input' name='email' type='text' value='pochta@yandex.ru' />
+        </div>
+      </form>
     </main>
   )
 }
