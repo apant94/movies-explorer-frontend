eeb6525606e080595345607d1c4e02c3b455081c
Anna Panteleeva <apanteleeva94@gmail.com>
2023-02-28 15:23:36 +0300

    feat: создает условные выражения для хедера

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,16 +14,30 @@ function Header({children}) {
   
   return (
     <>
-    {
-      pathname === '/' ? (
-    <header className='header' style={mainHeaderStyle}>
-      {children}
-    </header>
-    ) : 
-    (<header className='header' style={headerStyle}>
-      {children}
-    </header>)
-    }
+      {(pathname === '/signin' || pathname === '/signup' || pathname === '/404') && null}
+      {
+        pathname === '/' && (
+          <header className='header' style={mainHeaderStyle}>
+            {children}
+          </header>)
+      }
+      {
+        (pathname === '/movies' || pathname === '/saved-movies' || pathname === '/profile') && (
+        <header className='header' style={headerStyle}>
+            {children}
+        </header>)
+      }
+
+      {/* {
+        pathname === '/' ? (
+        <header className='header' style={mainHeaderStyle}>
+          {children}
+        </header>
+        ) : 
+        (<header className='header' style={headerStyle}>
+          {children}
+        </header>)
+      }  */}
     </>
   );
 }
