93b3d6b09ce279eea8cc9a7df06b651417eabf6d
Anna Panteleeva <apanteleeva94@gmail.com>
2023-02-28 12:55:21 +0300

    feat: добавляет структуру Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,35 @@
 import './Header.css';
+import { useLocation, Link } from 'react-router-dom';
+import LogoPath from '../../images/header-logo.svg';
 
 function Header() {
+  const {pathname} = useLocation();
+
+  const mainHeaderStyle = {
+    backgroundColor: '#F3C1F8',
+  }
+
+  const headerStyle = {
+    backgroundColor: '#FFFFFF',
+  }
+  
   return (
-  <header className='header'>
-    
-  </header>
+    <>
+    {
+      pathname === '/' ? (
+    <header className='header' style={mainHeaderStyle}>
+      <Link to='/'>
+        <img className='header__logo' src={LogoPath} alt='Логотип' />
+      </Link>
+    </header>
+    ) : 
+    (<header className='header' style={headerStyle}>
+      <Link to='/'>
+        <img className='header__logo' src={LogoPath} alt='Логотип' />
+      </Link>
+    </header>)
+    }
+    </>
   );
 }
 
