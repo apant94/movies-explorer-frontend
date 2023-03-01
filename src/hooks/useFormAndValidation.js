338cf9352e81f085b453e76eb751ecd67a634f62
Anna Panteleeva <apanteleeva94@gmail.com>
2023-03-01 17:03:24 +0300

    fix: упрощает хук

--- src/hooks/useFormAndValidation.js
+++ src/hooks/useFormAndValidation.js
@@ -3,35 +3,31 @@ import { useState, useCallback } from "react";
 export function useFormAndValidation() {
   const [values, setValues] = useState({});
   const [errors, setErrors] = useState({});
-  const [isValid, setIsValid] = useState(true);
+  const [isValid, setIsValid] = useState(false);
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const target = e.target;
+    const name = target.name;
+    const value = target.value;
     setValues({ ...values, [name]: value });
-    setErrors({ ...errors, [name]: e.target.validationMessage });
-    setIsValid(e.target.closest("form").checkValidity());
+    setErrors({ ...errors, [name]: target.validationMessage });
+    setIsValid(target.closest("form").checkValidity());
   };
 
   const resetForm = useCallback(
-    (newValues = {}) => {
+    (newValues = {}, newErrors = {}, newIsValid = false ) => {
       setValues(newValues);
+      setErrors(newErrors);
+      setIsValid(newIsValid);
     },
-    [setValues]
+    [setValues, setErrors, setIsValid]
   );
-
-  const resetValidation = useCallback(( newErrors = {}, newIsValid = false) => {
-    setErrors(newErrors);
-    setIsValid(newIsValid);
-  }, [setErrors, setIsValid] )
 
   return {
     values,
     handleChange,
     errors,
     isValid,
-    resetForm,
-    setValues,
-    setIsValid,
-    resetValidation
+    resetForm
   };
 }
