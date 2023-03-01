55905775bdbb65933eb4412334c166539e1f914c
Anna Panteleeva <apanteleeva94@gmail.com>
2023-03-01 16:35:18 +0300

    feat: стиль кнопки сабмит

--- src/components/AuthForm/AuthForm.js
+++ src/components/AuthForm/AuthForm.js
@@ -2,15 +2,16 @@ import './AuthForm.css';
 import { Link } from 'react-router-dom';
 import LogoPath from '../../images/header-logo.svg';
 
-function AuthForm({greetings, children}) {
+function AuthForm({greetings, children, submitText, onSubmit}) {
   return(
     <div className='authform'>
       <Link to='/'>
         <img src={LogoPath} alt='Логотип' className='authform__logo' />
       </Link>
       <h2 className='authform__greetings'>{greetings}</h2>
-      <form className='authform__element'>
+      <form className='authform__element' noValidate>
         {children}
+        <button className='authform__submit' type='submit' onSubmit={onSubmit}>{submitText}</button>
       </form>
     </div>
   )
