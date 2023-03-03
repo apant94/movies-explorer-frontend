6045d427a10ba22958edbf77640ac60a94c751f1
Anna Panteleeva <apanteleeva94@gmail.com>
2023-03-03 10:36:00 +0300

    feat: добавляет функциональность кнопке выйти и значения УК

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,22 +1,38 @@
 import './Profile.css';
+import { useState } from 'react';
 
 function Profile({logout}) {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  function handleSetEmail(e) {
+    setEmail(e.target.value);
+  }
+
+  function handleSetName(e) {
+    setName(e.target.value);
+  }
 
   return(
     <main className='profile'>
       <h2 className='profile__greetings'>Привет, Виталий!</h2>
-      <form className='profile__form'>
+      <form className='profile__form' onClick={handleSubmit}>
         <div className='profile__input-wrapper'>
           <label for='name' className='profile__input-label'>Имя</label>
-          <input className='profile__input' name='name' type='text' value='Виталий' />
+          <input className='profile__input' placeholder="Имя"name='name' type='text' value={name || ''} onChange={handleSetName} minLength={'2'} maxLength={'20'} />
         </div>
         <div className='profile__input-wrapper'>
           <label for='email' className='profile__input-label'>E-mail</label>
-          <input className='profile__input' name='email' type='text' value='pochta@yandex.ru' />
+          <input className='profile__input' placeholder="Почта" name='email' type='email' value={email || ''} onChange={handleSetEmail} />
         </div>
         <button type='submit' className='profile__edit-btn'>Редактировать</button>
       </form>
-      <button to type='button' className='profile__exit-btn' onClick={logout}>Выйти из аккаунта</button>
+      <button type='button' className='profile__exit-btn' onClick={logout}>Выйти из аккаунта</button>
     </main>
   )
 }
