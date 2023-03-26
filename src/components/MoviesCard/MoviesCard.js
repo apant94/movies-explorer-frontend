46e6efe348bc08c4397fced187e29e131026f751
Anna Panteleeva <apanteleeva94@gmail.com>
2023-03-26 17:09:21 +0300

    fix: исправляет тоггл лайка при нажатии

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,35 +1,19 @@
 import './MoviesCard.css';
-import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 function MoviesCard({ movie, savedMovie, onLikeClick, onDeleteClick }) {
-  const [ isLiked, setIsLiked ] = useState(false);
   const { pathname } = useLocation();
   const image = pathname === `${"/movies"}` ? `https://api.nomoreparties.co${movie.image.url}` : `${movie.image}`;
   const nameRU = pathname === `${"/saved-movies"}` ? `${movie.nameRU}` : `${movie.nameRU}`;
   const trailerLink = pathname === `${"/saved-movies"}` ? `${movie.trailerLink}` : `${movie.trailerLink}`;
   const duration = pathname === `${"/saved-movies"}` ? `${movie.duration}` : `${movie.duration}`;
 
-  const cardLikeButtonClassName = (
-    `moviescard__like ${isLiked ? 'moviescard__like_active' : 'moviescard__like'}`
-  );
-
-  function handleLikeClick(e) {
-    e.preventDefault();
-    e.target.classList.toggle('moviescard__like_active');
-    if (!isLiked) {
-      onLikeClick(movie);
-      setIsLiked(true);
-    } else {
-      onDeleteClick(movie);
-      setIsLiked(false);
-    };
+  function handleLikeClick() {
+    onLikeClick(movie);
   }
 
-  function handleDeleteClick(e) {
-    e.preventDefault();
+  function handleDeleteClick() {
     onDeleteClick(movie);
-    setIsLiked(false);
   }
 
   function convertTimeDuration(mins) {
@@ -43,7 +27,7 @@ function MoviesCard({ movie, savedMovie, onLikeClick, onDeleteClick }) {
       <div className='moviescard__container'>
         <h2 className='moviescard__name'>{nameRU}</h2>
         <p className='moviescard__duration'>{convertTimeDuration(duration)}</p>
-        {pathname === '/movies' && (<button onClick={handleLikeClick} className={cardLikeButtonClassName} type='button'></button>)}
+        {pathname === '/movies' && (<button onClick={savedMovie ? handleDeleteClick : handleLikeClick} className={`moviescard__like${savedMovie ? '_active' : ''}`} type='button'></button>)}
         {pathname === '/saved-movies' && (<button onClick={handleDeleteClick} className='moviescard__delete' type='button'></button>)}
       </div>
       <a href={trailerLink} target="_blank" rel="noreferrer">
